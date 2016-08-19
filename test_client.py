5a43a33f3523aed8f3fbebbe12afb4821269e3ca
Andrea Grandi <a.grandi@gmail.com>
2016-08-19 13:28:10 +0100

    Fix test for get_example

--- test_client.py
+++ test_client.py
@@ -4,7 +4,7 @@ import client
 
 
 class TestClient(TestCase):
-    @mock.patch('requests.request')
+    @mock.patch('requests.get')
     def test_get_example(self, mock_request):
         mocked_response = mock.Mock()
         mocked_response.status_code = 200
@@ -12,4 +12,4 @@ class TestClient(TestCase):
         mocked_response.json.return_value = expected_dict
         mock_request.return_value = mocked_response
         response = client.get_example()
-        self.assertEqual(response.json(), expected_dict)
+        self.assertEqual(response, expected_dict)
